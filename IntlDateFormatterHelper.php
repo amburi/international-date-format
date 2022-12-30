9a3d9eb8ac4cf9d5df5c21db6b9082e4aa8d2dbb
Amburi Roy <amburi.roy@gmail.com>
2022-12-30 03:20:12 +0100

    feat: added more parse and format methods in helper

--- IntlDateFormatterHelper.php
+++ IntlDateFormatterHelper.php
@@ -1,5 +1,7 @@
 <?php
 
+use Cassandra\Date;
+
 /**
  * International Date Formatter Helper
  */
@@ -15,22 +17,52 @@ class IntlDateFormatterHelper
     }
 
     /**
-     * International Date Formatter
+     * String-to-timestamp international date formatter
      *
-     * @param String $date
-     * @param String $fromFormat
-     * @param String $toFormat
+     * @param string $date
+     * @param string $fromFormat
+     * @return int
+     */
+    public function parseTimestamp(string $date, string $fromFormat): int
+    {
+        if (empty($date) || empty($fromFormat)) {
+            throw new InvalidArgumentException("Enter proper date");
+        }
+
+        $this->intlDateFormatter->setPattern($fromFormat);
+        return $this->intlDateFormatter->parse($date);
+    }
+
+    /**
+     * String-to-date object international date formatter
+     *
+     * @param string $date
+     * @param string $fromFormat
+     * @return Date
+     */
+    public function parseDate(string $date, string $fromFormat): Date
+    {
+        $timestamp = $this->parseTimestamp($date, $fromFormat);
+        return new Date($timestamp);
+    }
+
+    /**
+     * String-to-String international date formatter
+     *
+     * @param string $date
+     * @param string $fromFormat
+     * @param string $toFormat
      * @return string
      */
-    public function parseDate(string $date, string $fromFormat, string $toFormat): string
+    public function formatStrToStr(string $date, string $fromFormat, string $toFormat): string
     {
-        // convert international timestamp
-        $this->intlDateFormatter->setPattern($fromFormat);
-        $formatTimestamp = $this->intlDateFormatter->parse($date);
+        if (empty($date) || empty($fromFormat) || empty($toFormat)) {
+            throw new InvalidArgumentException("Enter proper date");
+        }
 
-        // now convert timestamp to any date format
+        $timestamp = $this->parseTimestamp($date, $fromFormat);
         $datetime = new DateTime();
-        $datetime->setTimestamp($formatTimestamp);
+        $datetime->setTimestamp($timestamp);
         return $datetime->format($toFormat);
     }
 
